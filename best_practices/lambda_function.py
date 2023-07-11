eff0a8e3a79529615b57935274c1df948098a42a
RexMudanya <musungumudanya@gmail.com>
2023-07-11 05:32:12 +0300

    feat: pytest unittests

---
+++ best_practices/lambda_function.py
@@ -0,0 +1,15 @@
+import os
+import model
+
+
+TEST_RUN = os.getenv('DRY_RUN', 'False') == 'True'
+PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME', 'ride_predictions')
+
+model_service = model.init(
+    prediction_stream_name=PREDICTIONS_STREAM_NAME,
+    RUN_ID = os.getenv('RUN_ID'),
+    test_run=TEST_RUN
+)
+
+def lambda_handler(event, context):
+    return model_service.lambda_handler(event)
---
+++ best_practices/model.py
@@ -0,0 +1,93 @@
+import json
+import base64
+import boto3
+import os
+import mlflow
+
+
+# logged_model = f"s3://mlflow-artifacts-remote-store-artifacts/lin_reg.bin"
+# model = mlflow.pyfunc.load_model(logged_model)
+
+# kinesis_client = boto3.client('kinesis')
+def load_model(run_id):
+    logged_model = f's3://mlflow-models-alexey/1/{run_id}/artifacts/model'
+    model = mlflow.pyfunc.load_model(logged_model)
+    return model
+
+
+def base64_decode(encoded_data):
+    decoded_data = base64.b64decode(encoded_data).decode('utf-8')
+    ride_event = json.loads(decoded_data)
+
+    return ride_event
+
+
+class ModelService():
+    def __init__(self, model, model_version=None, callbacks=None) -> None:
+        self.model = model
+        self.model_version = model_version
+        self.callbacks = callbacks or []
+
+    def prepare_features(self, ride):
+        features = {}
+        features['PU_DO'] = '%s_%s' % (ride["PULocationID"], ride["DOLocationID"])
+        features["trip_distance"] = ride["trip_distance"]
+        return features
+
+    def predict(self, features):
+        # X = dv.transform(feature)
+        # preds = model.predict(X)
+        pred = self.model.predict(features)
+        return float(pred[0])
+    
+    def lambda_handler(self, event, context):
+        predictions = []
+        for record in event['Records']:
+            encoded_data = record['kinesis']['data']
+            ride_event = base64_decode(encoded_data)
+            # print(ride_event)
+            ride = ride_event['ride']
+            ride_id = ride_event['ride_id']
+            
+            features = self.prepare_features(ride)
+            prediction = self.predict(features)
+            prediction_event = {
+                'model': 'ride_duration_prediction_model',
+                'version': self.model_version,
+                'prediction': {
+                    'ride_duration': prediction,
+                    'ride_id': ride_id
+                }
+            }
+            for callback in self.callbacks:
+                callback(prediction_event)
+            
+            predictions.append(prediction_event)
+            
+        return {
+            'predictions': predictions
+        }
+        
+class KinesisCallback():
+    def __init__(self, kinesis_client, PREDICTIONS_STREAM_NAME) -> None:
+        self.kinesis_client = kinesis_client
+        self.PREDICTIONS_STREAM_NAME = PREDICTIONS_STREAM_NAME
+    
+    def put_record(self, prediction_event):
+        ride_id = prediction_event['prediction']['ride_id']
+        self.kinesis_client.put_record(
+            StreamName = self.PREDICTIONS_STREAM_NAME,
+            Data=json.dumps(prediction_event),
+            PartitionKey=str(ride_id)
+        )
+
+
+def init(prediction_stream_name: str, run_id: str, test_run: bool):
+    model = load_model(run_id)
+    callbacks = []
+    if not test_run:
+        kinesis_client = boto3.client('kinesis')
+        KinesisCallback = KinesisCallback(kinesis_client, prediction_stream_name)
+        callbacks.append(KinesisCallback.put_record)
+    model_service = ModelService(model)
+    return model_service 
